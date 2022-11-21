963654388612adba4782da9eed31ef2173359852
realerdemdoruk <erdemdoruk3@gmail.com>
2022-11-21 20:45:28 +0300

    Bozuk boostrap kodu düzeltildi

--- src/components/GetCard.jsx
+++ src/components/GetCard.jsx
@@ -7,27 +7,31 @@ import '../style.css';
 
 const GetCard = () => {
   return (
-    <div className="row">
-      <h1 className="text-center mt-5">ATATÜRKÜN AİLESİ</h1>
-
-      {users.map((user) => (
-        <div className="col mt-3" key={user.id}>
-          <Card style={{ width: '18rem' }} className="shadow">
-            <Card.Img variant="top" src={user.img} />
-            <Card.Body>
-              <Card.Title>{user.name}</Card.Title>
-
-              <Card.Text>{user.desc}</Card.Text>
-
-              <Button variant="primary">
-                <Link className="Links" to="/family">
-                  Daha fazla bilgi
-                </Link>
-              </Button>
-            </Card.Body>
-          </Card>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1 className="text-center mt-5">ATATÜRKÜN AİLESİ</h1>
         </div>
-      ))}
+
+        <div className="col-md-12 d-flex justify-content-center flex-wrap">
+          {users.map((user) => (
+            <div className="mx-auto mt-3" key={user.id}>
+              <Card style={{ width: '18rem' }} className="shadow">
+                <Card.Img variant="top" src={user.img} />
+                <Card.Body>
+                  <Card.Title>{user.name}</Card.Title>
+                  <Card.Text>{user.desc}</Card.Text>
+                  <Button variant="primary">
+                    <Link className="Links" to="/family">
+                      Daha fazla bilgi
+                    </Link>
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
